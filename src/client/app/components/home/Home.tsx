236c23433f2c65931e02c80f7f7f0064e5696d82
KaFai Choi <kafaicoder@gmail.com>
2018-02-09 16:57:01 +0700

    create dummy form

--- src/client/app/components/home/Home.tsx
+++ src/client/app/components/home/Home.tsx
@@ -1,12 +1,47 @@
 import * as React from 'react'
+import { Note } from '../../../types/note'
 import NoteForm from '../note/form'
 
-function Home() {
-  return (
-    <div className="container home">
-      <NoteForm/>
-    </div>
-  )
+interface State {
+  note: Note
 }
 
-export default Home
+export class HomePageContainer extends React.Component<{}, State> {
+  public state = {
+    note: {
+      id: '-1',
+      title: 'Functional Programming 101',
+      body: 'Immutability...',
+    },
+  }
+
+  public render() {
+    return (
+      <div className="container home">
+        <NoteForm
+          note={this.state.note}
+          onChange={this.handleFieldValueChange}
+          onSave={this.handleSaveNote}
+        />
+      </div>
+    )
+  }
+
+  private handleFieldValueChange = (fieldName: string, value: string) => {
+    const nextState = {
+      ...this.state,
+      note: {
+        ...this.state.note,
+        [fieldName]: value,
+      },
+    }
+
+    this.setState(nextState)
+  }
+
+  private handleSaveNote = () => {
+    alert(1)
+  }
+
+}
+export default HomePageContainer
--- src/client/app/components/note/form.tsx
+++ src/client/app/components/note/form.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
+import { Note } from '../../../types/note'
+import { Button, Input } from '../common/form'
 
 interface Props {
+  note: Note
   onChange?: (fieldName: string, value: string) => void
   onSave?: () => void
 }
@@ -8,7 +11,25 @@ interface Props {
 const NoteForm: React.StatelessComponent<Props> = (props) =>
   (
     <form>
-      <h1>Create Form</h1>
+      <Input
+        name="title"
+        label="Note Title"
+        placeholder="What's your new note title?"
+        value={props.note.title}
+        onChange={props.onChange}
+      />
+      <Input
+        name="body"
+        label="Note body"
+        placeholder="What's your new note body?"
+        value={props.note.body}
+        onChange={props.onChange}
+      />
+      <Button
+        label="Save"
+        className="btn btn-default"
+        onClick={props.onSave}
+      />
     </form>
   )
 
